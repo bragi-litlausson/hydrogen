997406c8d230b0cdde280a821c337387f4d8667d
Bragi Litlausson <bragi@unstabledemo.space>
2023-03-29 20:33:31 +0200

    Create Horizontal Menu View files

---
+++ src/Hydrogen/Hydrogen/Views/HorizontalMenuView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Hydrogen.Views;
+
+public partial class HorizontalMenuView : UserControl
+{
+    public HorizontalMenuView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
