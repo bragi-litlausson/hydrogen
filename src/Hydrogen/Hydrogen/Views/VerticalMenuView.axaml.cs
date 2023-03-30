e1d49cf9219e2a9dbbbb16708f583dcec3fe769c
Bragi Litlausson <bragi@unstabledemo.space>
2023-03-30 17:27:12 +0200

    Add Vertical Menu Layout

---
+++ src/Hydrogen/Hydrogen/Views/VerticalMenuView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Hydrogen.Views;
+
+public partial class VerticalMenuView : UserControl
+{
+    public VerticalMenuView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
